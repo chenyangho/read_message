89bf5209500a2b2de64f9ae68709e7c8599c4d9b
Jacky <jacky85031085@gmail.com>
2020-11-12 06:54:04 +0900

    count < 100

--- read_m.py
+++ read_m.py
@@ -5,13 +5,15 @@ words_len = 0
 with open('reviews.txt', 'r') as file:
 	for message in file:
 		data.append(message.strip())
-		count += 1
-		if count % 1000 == 0:
-			print(len(data))
+		#count += 1
+		#if count % 1000 == 0:
+		#	print(len(data))
 			
 print("finish reading. there is", len(data))
 
 for d in data:
 	words_len += len(d)
+	if len(d) < 100:
+		print(d, "\n")
 
 print("average = ", words_len / len(data))
